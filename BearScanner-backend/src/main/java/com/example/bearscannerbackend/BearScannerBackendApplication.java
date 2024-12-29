86e3c7423e734d11c1678acda883eba3b94b4e10
blueconecell <95961799+blueconecell@users.noreply.github.com>
2024-12-29 20:52:28 +0900

    feat: project setting
    - spring boot

---
+++ BearScanner-backend/src/main/java/com/example/bearscannerbackend/BearScannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bearscannerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearScannerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BearScannerBackendApplication.class, args);
+    }
+
+}
